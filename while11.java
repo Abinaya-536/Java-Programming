17fb1b26ece07ff89bb606d5b4b30fe76d0ba734
Abinaya Kanagaraj <k.abinaya7kanagaraj@gmail.com>
2026-03-17 20:55:00 +0530

    Add files via upload
    solved java problems

---
+++ while11.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class GCDCalculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        int temp;
+
+        while (b != 0) {
+            temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        System.out.println("GCD of " + a + " and " + b + " = " + a);
+    }
+}

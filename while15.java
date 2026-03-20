d029e7e6c1cfce078d995a72b772034fb607254b
Abinaya Kanagaraj <k.abinaya7kanagaraj@gmail.com>
2026-03-20 20:53:17 +0530

    Add files via upload
    solved java problems

---
+++ while15.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class CollatzSequence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int count = 0;
+
+        System.out.println("Collatz sequence starting from " + n + ":");
+
+        do {
+            System.out.print(n + " ");
+
+            if (n == 1) {
+                break;
+            }
+
+            if (n % 2 == 0) {
+                n = n / 2;
+            } else {
+                n = 3 * n + 1;
+            }
+
+            count++;
+
+        } while (n > 0);
+
+        System.out.println("\nSteps: " + count);
+    }
+}

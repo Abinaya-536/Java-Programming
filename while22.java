52bf175f802fd4181cce06a85b9bc5446858f19f
Abinaya Kanagaraj <k.abinaya7kanagaraj@gmail.com>
2026-03-24 20:14:50 +0530

    Add files via upload
    solved java problems

---
+++ while22.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class MultiplicationTableDoWhile {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = sc.nextInt();
+        int range = sc.nextInt();
+
+        int i = 1;
+
+        System.out.println("Multiplication table of " + number + ":");
+
+        do {
+            System.out.println(number + " x " + i + " = " + (number * i));
+            i++;
+        } while (i <= range);
+
+        sc.close();
+    }
+}
